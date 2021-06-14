5ade3aef4d4f0f55d97cbd8423c9c7b87c077359
ch744 <christianfagundes21@gmail.com>
2021-06-14 12:28:53 -0300

    swap

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,29 +5,44 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Mao[] maos = Importador_CSV.CSV_reader("C:/dominos.csv");
+        Mao[] maos = Importador_CSV.CSV_reader("C:/Users/Pichau/Documents/dominos.csv");
 
+        /*
+         * Domino a = new Domino(3, 4); Domino b = new Domino(4, 3); Domino[] c = new
+         * Domino[2]; c[0] = a; c[1] = b;
+         */
 
-        Domino a = new Domino(3, 4);
-        Domino b = new Domino(4, 3);
-        Domino[] c = new Domino[2];
-        c[0] = a;
-        c[1] = b;
         Mao mao = new Mao();
-//        mao.somaPosicao(0, mao);
+        // mao.somaPosicao(0, mao);
 
-        for (int i = 0; i < maos.length; i++){
-            System.out.println(mao.verificaMatch(maos[i]));;
+        for (int i = 0; i < maos.length; i++) {
+            System.out.println(mao.verificaMatch(maos[i]));
+            
+            // faz o swap caso necessario
+            mao.setQuant(3);
+            
+            
+            int pos = 0;
+            while (mao.verificaMatch(maos[i]) == false && pos <= mao.getQuant()) {
+
+                System.out.println("swap " + "posição: " + pos);
+                mao.swap(maos[i], pos);
+                System.out.println(mao.verificaMatch(maos[i]));
+                if (mao.verificaMatch(maos[i]) == false) {
+                    mao.swap(maos[i], pos);
+                }
+                pos++;
+            }
+            
+            
+           
+           
         }
 
+    }
 
-
-
-
-
-
-
-
+    public void name() {
 
     }
+
 }
--- src/com/company/Mao.java
+++ src/com/company/Mao.java
@@ -17,39 +17,43 @@ public class Mao {
 
     public Integer getQuant() {
         return quant;
+       
+    }
+
+    public void setQuant(int quant) {
+        this.quant = quant;
     }
 
     public Domino[] getMao() {
         return mao;
     }
 
-
-
-    public boolean verificaSeEhPar(Domino[] dominos){
+    public boolean verificaSeEhPar(Domino[] dominos) {
         int soma = 0;
-        for (int i = 1; i <dominos.length; i++){
+        for (int i = 1; i < dominos.length; i++) {
             soma += dominos[i].getSoma();
         }
-        if(soma % 2 == 0){
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public boolean verificaMatch(Mao mao){
-        if (somaPosicao(0, mao) == somaPosicao(1, mao)){
+        if (soma % 2 == 0) {
             return true;
         } else {
             return false;
         }
     }
 
-    public Integer[] somaMao(Mao mao){
+    public boolean verificaMatch(Mao mao) {
+        if (somaPosicao(0, mao) == somaPosicao(1, mao)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Integer[] somaMao(Mao mao) {
         int somaCima = 0;
         int somaBaixo = 0;
         Domino[] dominos = mao.getMao();
 
-        for (int i = 0; i < dominos.length; i++){
+        for (int i = 0; i < dominos.length; i++) {
             somaCima += dominos[i].getDomino()[0];
             somaBaixo += dominos[i].getDomino()[1];
         }
@@ -59,23 +63,27 @@ public class Mao {
         return response;
     }
 
-    //soma cima ou baixo, passando em posicao
-    public Integer somaPosicao(int posicao, Mao mao){
+    // soma cima ou baixo, passando em posicao
+    public Integer somaPosicao(int posicao, Mao mao) {
         int soma = 0;
         Domino[] dominos = mao.getMao();
 
-        for (int i = 0; i < dominos.length; i++){
+        for (int i = 0; i < dominos.length; i++) {
             soma += dominos[i].getDomino()[posicao];
         }
         return soma;
     }
 
+    public void swap(Mao mao, int pos) {
+
+        Domino[] dominos = mao.getMao();
+
+        dominos[0].swap(dominos[pos]);
+
+    }
 
     @Override
     public String toString() {
-        return "Mao{" +
-                "quant=" + quant +
-                ", mao=" + Arrays.toString(mao) +
-                '}';
+        return "Mao{" + "quant=" + quant + ", mao=" + Arrays.toString(mao) + '}';
     }
 }
